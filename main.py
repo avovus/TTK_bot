7784a68aea7091952768e96353304168677ee670
despoyno <gffmscedm@gmail.com>
2024-10-27 04:12:11 +0300

    main.py
    Работает войс модуль и подгружены номера договоров, на которые он реагирует

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 
 c = 0
 
+VALID_CODES = {"516000001", "516000002", "516000003", "516000004", "516000005", "516000006", "516000007", "516000008", "516000009"}
+needCheckCode = False
 TOKEN = "8111640966:AAH4GeOlltSX7Qnu0woBD4voyEzLkqNm4b0"
 model_path = "C://Users//Eden Despoyno//Desktop//vosk-model-ru-0.22" #тут должен быть путь к локальной модели
 messages = {}
@@ -48,6 +50,7 @@ async def handle_voice_message(update: Update, context: CallbackContext) -> None
     voice_file = await context.bot.get_file(update.message.voice.file_id)
     voice_file_path = "voice_message.ogg"
     global c
+    global needCheckCode
     file_url = voice_file.file_path
     response = requests.get(file_url)
     with open(voice_file_path, "wb") as f:
@@ -57,23 +60,29 @@ async def handle_voice_message(update: Update, context: CallbackContext) -> None
     os.system(f"ffmpeg -i {voice_file_path} -ar 16000 -ac 1 {wav_file_path}")
     wav_file_path = "voice_message.wav"
     recognized_text = voice_to_text(wav_file_path)
-    #await update.message.reply_text(f"Распознанный текст: {recognized_text}")
 
     message = recognized_text
-    if "тариф" in message:
+    if needCheckCode and message in VALID_CODES:
+        await update.message.reply_text(messages.get("yescon"))
+        needCheckCode = False
+    elif needCheckCode and not(message in VALID_CODES):
+        await update.message.reply_text(messages.get("nocon"))
+        needCheckCode = False
+    elif "тариф" in message:
         await update.message.reply_text(messages.get("tariffs", "Вот тарифы на выбор:"))
     elif "клиент" in message:
         await update.message.reply_text(messages.get("client", "Добро пожаловать!"))
     elif "договор" in message:
         await update.message.reply_text(messages.get("contract", "Введите данные"))
+        needCheckCode = True
     elif "поддержка" in message:
-        await update.message.reply_text(messages.get("hotline", "Введите данные"))
+        await update.message.reply_text(messages.get("hotline", "номера поддержки"))
     elif "привет" in message:
         await update.message.reply_text(messages.get("greetings", "Привет!"))
     elif "пока" in message:
         await update.message.reply_text(messages.get("farewell", "До свидания!"))
     else:
-        await update.message.reply_text(messages.get("error", "ошибка"))
+        await update.message.reply_text(messages.get("error", "ошибули"))
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -97,15 +106,23 @@ async def verify_contract(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
         await update.message.reply_text("nocon")
 
 async def handle_intent(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global needCheckCode
     message = update.message.text.lower()
-    if "тариф" in message:
+    if needCheckCode and message in VALID_CODES:
+        await update.message.reply_text(messages.get("yescon"))
+        needCheckCode = False
+    elif needCheckCode and not (message in VALID_CODES):
+        await update.message.reply_text(messages.get("nocon"))
+        needCheckCode = False
+    elif "тариф" in message:
         await update.message.reply_text(messages.get("tariffs", "Вот тарифы на выбор:"))
     elif "клиент" in message:
         await update.message.reply_text(messages.get("client", "Добро пожаловать!"))
     elif "договор" in message:
         await update.message.reply_text(messages.get("contract", "Введите данные"))
+        needCheckCode = True
     elif "поддержка" in message:
-        await update.message.reply_text(messages.get("hotline", "Введите данные"))
+        await update.message.reply_text(messages.get("hotline", "номера поддержки"))
     elif "привет" in message:
         await update.message.reply_text(messages.get("greetings", "Привет!"))
     elif "пока" in message:
